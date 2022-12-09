346dcb6744af8d09a210b2dd76b86f37f979c8e4
aswinmargy16 <aswinmargy16@gmail.com>
2022-12-09 11:31:59 +0530

    STATE

---
+++ stateprops/src/App.js
@@ -0,0 +1,12 @@
+import Message from './Component/message'
+import Navbar from './Navbar/navbar'
+let App = () => {
+    return <div>
+        <Navbar />
+        <hr />
+        <Message />
+    </div>
+
+}
+
+export default App
---
+++ stateprops/src/Navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return <nav className="navbar navbar-dark bg-dark">
+            <a href="#" className="navbar-brand">ReAcT State ExAmPlE</a>
+        </nav>
+    }
+}
+
+export default Navbar
